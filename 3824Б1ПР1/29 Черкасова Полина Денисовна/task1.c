d1b68cafd67e58537dd1bb5220acb1b9e81b6e6b
Полина Черкасова <127506525+cherkaasova@users.noreply.github.com>
2024-12-19 09:21:44 +0300

    Update task1.c

--- 3824Б1ПР1/29 Черкасова Полина Денисовна/task1.c
+++ 3824Б1ПР1/29 Черкасова Полина Денисовна/task1.c
@@ -1,6 +1,11 @@
 // Задача 1. Дан непустой массив A, содержащий n целых чисел.
 // Найти номер его последнего минимального элемента.
+#include<stdio.h>
 int task1(int A[], size_t n)
 {
-  return -1;
+	int mintek = 0;
+	for (int i = 1;i < n;i++) {
+		if (A[i] < A[mintek]) mintek = i;
+	}
+	return mintek;
 }
