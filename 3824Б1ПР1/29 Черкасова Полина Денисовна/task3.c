3379fd5076648a1d3f575b4b1a646b055d43bb04
Полина Черкасова <127506525+cherkaasova@users.noreply.github.com>
2024-12-19 10:15:32 +0300

    Update task3.c

--- 3824Б1ПР1/29 Черкасова Полина Денисовна/task3.c
+++ 3824Б1ПР1/29 Черкасова Полина Денисовна/task3.c
@@ -4,7 +4,18 @@
 //   то включить в ss N последних символов строки s.
 // Если длина строки s меньше N,
 //   то включить в ss все символы строки s, добавив перед ними символы '*'.
-char* task3(char *s, unsigned N)
-{
-  return "task3";
+#include <stdio.h>
+#include <string.h>
+
+char* task3(char* ss, const char* s, size_t N) {
+    size_t len_s = strlen(s); 
+
+    if (len_s >= N) {
+        strncpy(ss, s + len_s - N, N);
+        ss[N] = '\0';  
+    }
+    else {
+        memset(ss, '*', N - len_s);
+        memcpy(ss + N - len_s, s, len_s + 1);
+    }
 }
