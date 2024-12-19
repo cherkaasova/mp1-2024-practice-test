6d01f3f49c2625ad4b76a999085b6c9dbf709761
Полина Черкасова <127506525+cherkaasova@users.noreply.github.com>
2024-12-19 09:40:16 +0300

    Update task2.c

--- 3824Б1ПР1/29 Черкасова Полина Денисовна/task2.c
+++ 3824Б1ПР1/29 Черкасова Полина Денисовна/task2.c
@@ -2,7 +2,14 @@
 // Найти значение минимального из его локальных максимумов.
 // Локальный максимум – элемент, который >= левого и правого соседей.
 // При отсутствии локальных максимумов вернуть -1.
-long long task2(unsigned A[], size_t n)
-{
-  return -2;
-}
+#include <stdio.h>
+#include<limits>
+
+long long task2(unsigned A[], size_t n) {
+    if (n < 3) return -2;
+    int min_local = LLONG_MAX;
+    for (int i = 1; i < n - 1; ++i) {
+        if (A[i] >= A[i - 1] && A[i] >= A[i + 1]) {
+            if (A[i] < min_local) min_local = A[i];}}
+    if (min_local == LLONG_MAX)return -1;
+    return min_local;}
